53411bd2fa55b8354ff4e89dbc4aff8982e3b0a7
Aldo Arista <aristaaldo2@gmail.com>
2025-08-06 16:14:50 +0700

    feat(CI-25): added register page mvp

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -1,7 +1,51 @@
 export default function RegisterPage() {
   return (
     <>
-      <h1>Register page</h1>
+      <div className="mb-8 text-center">
+        <h1>Cerdas Isyarat</h1>
+        <h2>Belajar Bahasa Isyarat Asik</h2>
+      </div>
+
+      <div className="mx-auto flex w-2/4 flex-col justify-center rounded-xl border p-8">
+        <div className="mb-4 text-center">Daftar</div>
+        <form action="">
+          <div className="mb-4 flex flex-col gap-2">
+            <input
+              type="text"
+              placeholder="nama lengkap"
+              className="border p-2"
+            ></input>
+            <input
+              type="text"
+              placeholder="umur"
+              className="border p-2"
+            ></input>
+            <input
+              type="text"
+              placeholder="username"
+              className="border p-2"
+            ></input>
+            <input
+              type="text"
+              placeholder="password"
+              className="border p-2"
+            ></input>
+            <input
+              type="text"
+              placeholder="konfirmasi password"
+              className="border p-2"
+            ></input>
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="w-full rounded-lg border bg-gray-100 p-2"
+            >
+              buat akun
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
