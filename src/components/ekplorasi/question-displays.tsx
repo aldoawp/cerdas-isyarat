176c0a99c861c160446210a05c8532ec6a129dbf
Aldo Arista <aristaaldo2@gmail.com>
2025-09-13 00:58:58 +0700

    feat: added exploration assessment feature

--- src/components/ekplorasi/question-displays.tsx
+++ src/components/ekplorasi/question-displays.tsx
@@ -4,17 +4,28 @@ import Image from 'next/image';
 
 export const QuestionDisplay = ({
   questionAsset,
+  questionText,
 }: {
   questionAsset: string;
+  questionText?: string;
 }) => (
-  <div className="mx-auto flex h-48 w-full max-w-md items-center justify-center rounded-2xl border-2 border-input-border bg-input-bg p-4 shadow-inner">
-    <Image
-      src={questionAsset}
-      alt="Pertanyaan"
-      width={250}
-      height={250}
-      unoptimized
-      className="max-h-40 max-w-full rounded-xl object-contain"
-    />
+  <div className="space-y-4">
+    <div className="mx-auto flex h-48 w-full max-w-md items-center justify-center rounded-2xl border-2 border-input-border bg-input-bg p-4 shadow-inner">
+      <Image
+        src={questionAsset}
+        alt="Pertanyaan"
+        width={250}
+        height={250}
+        unoptimized
+        className="max-h-40 max-w-full rounded-xl object-contain"
+      />
+    </div>
+    {questionText && (
+      <div className="mx-auto max-w-md text-center">
+        <p className="text-lg font-semibold text-brand-brown-stroke">
+          {questionText}
+        </p>
+      </div>
+    )}
   </div>
 );
